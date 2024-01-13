0bf4f99b18b433158ab6ad94a5ed91b6f059dbe8
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2024-01-13 09:10:15 +0700

    set system bars color

--- app/src/main/java/me/hib4/wejanganmadyo/MainActivity.kt
+++ app/src/main/java/me/hib4/wejanganmadyo/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import me.hib4.wejanganmadyo.presentation.navgraph.NavGraph
 import me.hib4.wejanganmadyo.ui.theme.WejanganMadyoTheme
@@ -29,6 +33,16 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             WejanganMadyoTheme {
+                val isDarkMode = isSystemInDarkTheme()
+                val systemUiController = rememberSystemUiController()
+
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isDarkMode
+                    )
+                }
+
                 Box(
                     modifier = Modifier.background(MaterialTheme.colorScheme.background)
                 ) {
